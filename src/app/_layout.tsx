b086c20370a56ac99736a8a38a601368deeba5f7
Francis Lagares <francis.lagares@gmail.com>
2024-11-17 19:35:37 +0100

    feat: clerk authentication setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,50 @@
-import { Stack } from 'expo-router';
+import { tockenCache } from '@/utils/cache';
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
+import {
+  DMSans_400Regular,
+  DMSans_500Medium,
+  DMSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/dm-sans';
+import { Slot, SplashScreen } from 'expo-router';
+import { useEffect } from 'react';
+import { LogBox } from 'react-native';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  );
+}
+
+LogBox.ignoreLogs(['Clerk: Clerk has been loaded with development keys']);
+
+// Prevent auto hide splash screen
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSans_500Medium,
+    DMSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  return <Slot />;
+};
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name='index' />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tockenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
