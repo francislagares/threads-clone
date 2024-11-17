60414370f831bdc99a839540bb12dad9b6ca045c
Francis Lagares <francis.lagares@gmail.com>
2024-11-17 21:52:51 +0100

    feat: login screen with clerk authentication

--- src/app/(public)/index.tsx
+++ src/app/(public)/index.tsx
@@ -1,15 +1,156 @@
-import { Text, View } from 'react-native';
+import { Colors } from '@/constants/Colors';
+import { useOAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function Index() {
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
+  const { startOAuthFlow: facebookAuth } = useOAuth({ strategy: 'oauth_facebook' });
+
+  const handleFacebookLogin = async () => {
+    try {
+      const { createdSessionId, setActive } = await facebookAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const { createdSessionId, setActive } = await googleAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello Threads !</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('@/assets/images/login.png')}
+        style={styles.loginImage}
+      />
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>How would you like to use Threads ?</Text>
+        
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.loginButton} onPress={handleFacebookLogin}>
+            <View style={styles.loginButtonContent}>
+              <Image
+                source={require('@/assets/images/instagram_icon.webp')}
+                style={styles.loginButtonIcon}
+              />
+              <Text style={styles.loginButtonText}>Continue with Instagram</Text>
+              <Ionicons name="chevron-forward" size={24} color={Colors.border} />
+            </View>
+            <Text style={styles.loginButtonSubtitle}>
+              Log in or create a THreads profile with your Instagram account. With a profile, you
+              can post, interact and get personalised recommendations.
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={handleGoogleLogin}
+          >
+            <View style={styles.loginButtonContent}>
+              <Text style={styles.loginButtonText}>Continue with Google</Text>
+              <Ionicons
+                name='chevron-forward'
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.loginButton}>
+            <View style={styles.loginButtonContent}>
+              <Text style={styles.loginButtonText}>Use without a profile</Text>
+              <Ionicons
+                name='chevron-forward'
+                size={24}
+                color={Colors.border}
+              />
+            </View>
+            <Text style={styles.loginButtonSubtitle}>
+              You can browse Threads without a profile, but won't be able to
+              post, interact or get personalised recommendations.
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.switchAccountButtonText}>Switch accounts</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    alignItems: 'center',
+    backgroundColor: Colors.background,
+  },
+  loginImage: {
+    width: '100%',
+    height: 350,
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 17,
+    fontFamily: 'DMSans_500Medium',
+  },
+  loginButton: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderRadius: 8,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.border,
+  },
+  buttonContainer: {
+    gap: 20,
+  },
+  loginButtonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  loginButtonIcon: {
+    width: 50,
+    height: 50,
+  },
+  loginButtonText: {
+    color: '#000',
+    fontSize: 15,
+    fontFamily: 'DMSans_500Medium',
+    flex: 1,
+  },
+  loginButtonSubtitle: {
+    fontSize: 12,
+    fontFamily: 'DMSans_400Regular',
+    color: '#acacac',
+    marginTop: 5,
+  },
+  switchAccountButtonText: {
+    fontSize: 14,
+    color: Colors.border,
+    alignSelf: 'center',
+  },
+});
